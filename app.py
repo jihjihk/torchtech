79fb7f7ad26a08cc40c0278e2a7f44035a22e6e0
Jihyun <jihyun@nyu.edu>
2017-06-24 23:25:14 -0400

    css

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     author = "Me"
     name = "Jihyun"
     return render_template('index.html', author=author, name=name)
 
+@app.route('/elements', methods=['GET', 'POST'])
+def urgent_btn():
+	if request.method == 'POST':
+		return redirect(url_for('index'))
+	return render_template('urgent.html')
+
+@app.route('/danger', methods=['GET', 'POST'])
+def danger_btn():
+	if request.method == 'POST':
+		return redirect(url_for('index'))
+	return render_template('danger.html')
+
+@app.route('/safewalk', methods=['GET', 'POST'])
+def safewalk_btn():
+	if request.method == 'POST':
+		return redirect(url_for('index'))
+	return render_template('safewalk.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
